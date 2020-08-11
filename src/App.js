75cab6e45cab7ae85f6e7b539da456677f5c1a6a
Carine Batista <carinedcb@hotmail.com>
2020-08-11 01:09:55 -0300

    Add components button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+import AddButton from './components/AddButton';
+import List from './components/List';
+
+
 function App() {
+  const [items, setItems] = useState( ['Banana', 'Mamão', 'Abacaxi'] )
+
   return (
     <div className="App">
-        <h1> to Do list</h1>
+        <h1> To Do list</h1>
+        <List>
+        { 
+          items.map(item => {
+            return <li> {item}</li>
+          })
+         }
+
+        </List> 
+        <AddButton/>
+        
     </div>
   );
 }
---
+++ src/components/AddButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './AddButton.css';
+
+const AddButton = () =>{
+    return <button> 
+    Clique Aqui 
+    </button>
+}
+
+export default AddButton
